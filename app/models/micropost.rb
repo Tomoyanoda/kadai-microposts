a0e8baf8c45cddc90d9a466be5a8a99caf78eb8a
tomoya.noda <sakashimayui@gmail.com>
2020-02-08 08:03:54 +0000

    like, unlike

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,20 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum:255 }
+  
+  has_many :favorites, dependent: :destroy
+  has_many :liked_users, through: :favorites, source: :user
+
+  def like(user)
+    self.favorites.find_or_create_by(user_id: user.id)
+  end
+  
+  def unlike(user)
+    favorite = favorites.find_by(user_id: user.id)
+    favorite.destroy if favorite
+  end
+
+  def like?(user)
+    self.liked_users.include?(user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :liked_microposts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -24,9 +26,8 @@ class User < ApplicationRecord
   def following?(other_user)
     self.followings.include?(other_user)
   end
-  
+
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
-  
 end
